1ed5e5e3b1d2a92b9d0a974f2e6e0adf35e8e41a
Chandu Bayyavarapu <116875885+Chandu106@users.noreply.github.com>
2023-06-09 14:17:13 +0530

    Add files via upload

--- my_first_node.py
+++ my_first_node.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
+#creating a node using classes (oop)
 import rclpy #callback functionality
 from rclpy.node import Node
 
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("py_test")
+        self.get_logger().info("Hello ROOT")
+
 def main(args = None):
     rclpy.init(args=args) #initializing ros2 communications
-    node = Node("py_test") #creating the node with name py_test
-    node.get_logger().info("Hello ROOT") #for displaying/showing the information
+    node = MyNode()
     rclpy.spin(node)#in simple terms it is like a loop where the node gets spinned when we provide this functionality
     rclpy.shutdown() #lastline of the program to stop the communicatioon
 
