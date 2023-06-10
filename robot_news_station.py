0a6d992b9e7540c3b01a69b1184ed550a5044c67
Chandu Bayyavarapu <116875885+Chandu106@users.noreply.github.com>
2023-06-10 11:33:21 +0530

    Add files via upload

---
+++ robot_news_station.py
@@ -0,0 +1,31 @@
+#template
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node 
+
+from example_interfaces.msg import String
+
+class RobotNewsStationNode(Node):
+
+    def __init__(self):
+        super().__init__("robot_news_station")#name of the node
+
+        self.publisher = self.create_publisher(String, "robot_news", 10) #topic name is robot news
+        self.timer = self.create_timer(0.5, self.publish_news)
+        self.get_logger().info("Robot News Station has been started")
+
+    def publish_news(self):
+        msg = String()
+        msg.data = "hello ROOT"
+        self.publisher.publish(msg)
+
+ 
+def main(args=None):
+    rclpy.init(args=args)
+    node = RobotNewsStationNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__=="__main__":
+    main()
